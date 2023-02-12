d2d1be45a1695f216847ccd590f9df9ed726c2e6
ganqiyin <ganqiyin2009@163.com>
2023-02-12 21:11:00 +0800

    Update elementui.js

--- src/plugins/elementui.js
+++ src/plugins/elementui.js
@@ -3,7 +3,7 @@ import { Buttion, Switch } from "element-plus";
 export default {
     // 参数app：由Vue的creatApp生成的app
     // 参数options：用户传入的选项
-    install: (app,options) => {
+    install: (app) => {
         app.use(Buttion);
         app.use(Switch);
     },
