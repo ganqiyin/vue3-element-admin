c5ddb67038b7a64f76c13ac836854dbc042776eb
ganqiyin <ganqiyin2009@163.com>
2023-02-13 23:04:16 +0800

    添加登录页面

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,3 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
-  <el-button type="primary">主要按钮</el-button>
+  <router-view /> 
 </template>
---
+++ src/views/account/LoginView.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="login_wrap">
+      <div class="form_wrap">
+        <el-form ref="formRef" :model="loginData" label-width="100px" class="demo-dynamic">
+          <el-form-item prop="username" label="用户名" 
+            :rules="[
+              {
+                required: true,
+                message: '此项为必填项',
+                trigger: 'blur',
+              },
+            ]"
+          >
+            <el-input v-model="loginData.username" />
+          </el-form-item>
+  
+          <el-form-item prop="password" label="密码"
+            :rules="[
+              {
+                required: true,
+                message: '此项为必填项',
+                trigger: 'blur',
+              },
+            ]"
+          >
+            <el-input type="password" v-model="loginData.password" />
+          </el-form-item>
+  
+          <el-form-item>
+            <el-button type="primary" @click="submitForm(formRef)">登录</el-button>
+            <!-- <el-button @click="addDomain">New domain</el-button>
+                 <el-button @click="resetForm(formRef)">Reset</el-button> -->
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+    import { reactive, toRefs } from "vue";
+    export default {
+      name: "LoginView",
+      setup() {
+        const data = reactive({
+          loginData: {
+            username: "",
+            password: "",
+          },
+        });
+        return {
+          ...toRefs(data),
+        };
+      },
+    };
+  </script>
+
+  <style scoped>
+    .login_wrap {
+      width: 100%;
+      height: 100vh;
+      background: #2d3761;
+      position: relative;
+    }
+
+    .form_wrap {
+      position: fixed;
+      top: 50%;
+      transform: translate(130%, -50%);
+      background: #fff;
+      padding: 30px 50px;
+      border-radius: 5px;
+    }
+  </style>
